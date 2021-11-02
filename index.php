09d0cdf3773f3bf86e179cec97a641f7750c6092
ilmane-abdallah <92577708+ilmane-abdallah@users.noreply.github.com>
2021-11-02 16:55:28 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,6 +1,26 @@
+
 <?php
 
-$hello="Hello LaPlateforme";
-echo $hello;
+$str = "LaPlateforme" ;
+
+$str2 = "Vive" ;
+
+$str3 = "!" ;
+
+echo $str2 . " " . $str . " " . $str3 ;
+
+$val= 6 + 4;
+
+echo $val;
+
+$mybool ="true";
+
+echo $mybool;
+
+$mybool ="false";
+
+echo $mybool;
+
+
 
 ?>

2dbe1629c4809938864d8e453b870fe07c0da40e
ilmane-abdallah <92577708+ilmane-abdallah@users.noreply.github.com>
2021-11-02 17:49:12 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,53 +1,14 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>index.php</title>
-</head>
-<body>
-    <table>
-    <tr>
-    <th>Type</th>
-    <th>Nom</th>
-    <th>Valeur</th>  
-</tr>
-    <tr>
+<?php
 
-    <td>chaine de caractères</td>
-    <td>string</td>
-    <td>Ilmane</td>
-</tr>
 
-<tr>
 
-<td>Nombre entiers</td>
-<td>int</td>
-<td>3</td>
-</tr>
 
-<tr>
+$hello="hello LaPlateforme!";//1ere text
 
-<td>Nombre décimaux</td>
-<td>float</td>
-<td>14,459</td>
-</tr>
+echo $hello;
 
-<tr>
+$hello="hello LaPlateforme!";//2eme text
 
-<td>Booléens</td>
-<td>bool</td>
-<td>true or false</td>
-</tr>
+echo $hello;
 
-<tr>
 
-<td>Rien</td>
-<td>NULL</td>
-<td>Null</td>
-</tr>
-</table>
-
-</body>
-</html>

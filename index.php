2cdcafd00a83d8ac9452faeacb5627a52c11f38b
ilmane-abdallah <92577708+ilmane-abdallah@users.noreply.github.com>
2021-11-03 20:50:04 +0100

    Add files via upload
    jo1 42 gras.

--- index.php
+++ index.php
@@ -1,55 +1,11 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>index.php</title>
-</head>
 <?php
-    $booléens = true;
-    $chaineDeCaractere = 'Variable chaine de caractère';
-    $int = 20;
-    $float = 20.5;
-?>
-<body>
-    <table>
-    <tr>
-    <th>Type</th>
-    <th>Nom</th>
-    <th>Valeur</th>  
-</tr>
-    <tr>
 
-    <td>string</td>
-    <td> 'Variable chaine de caractère'</td>
-    <td><?= $chaineDeCaractere ?></td>
-</tr>
-
-<tr>
-
-<td>int</td>
-<td>entiers</td>
-<td><?=$int?></td>
-</tr>
-
-<tr>
-
-<td>float</td>
-<td>nombre decimaux</td>
-<td><?=$float?></td>
-</tr>
-
-<tr>
-
-<td>bool</td>
-<td>booléens</td>
-<td><?=$booléens ?></td>
-
-</tr>
-
-
-</table>
-
-</body>
-</html>
+for($i=0;$i<=1337;$i++) {
+    if($i == 42) {
+        echo ('<b><u>'. $i .'</u></b>');
+    }
+    else {
+        echo $i;
+    }
+    echo "<br/>";
+}

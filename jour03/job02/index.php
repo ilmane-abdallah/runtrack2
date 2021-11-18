9db5331de53f1268b7fd2665d086985808055b80
ilmane <ilmane.abdallah@plateforme.io>
2021-11-18 12:48:45 +0100

    jour03/job02/index.php saute un caractére

--- jour03/job02/index.php
+++ jour03/job02/index.php
@@ -1,8 +1,18 @@
 	
 <?php
 
-$colors=['blue','red','black','yellow'],
-  
+
+
+$str="Tous ces instants seront
+perdus dans le temps comme les larmes sous la pluie.";
+
+
+   for($i=0;$i<=(isset($str[$i]));$i++){
+       if($i % 2 == 0){
+           echo $str[$i];
+       }
+   }
+
 
 
 ?>

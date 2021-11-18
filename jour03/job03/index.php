0baaffb0b207ecfe32622dbd9316dfd6f3d04ce1
ilmane <ilmane.abdallah@plateforme.io>
2021-11-18 13:48:36 +0100

    jour03/job03/index.php voyelle

--- jour03/job03/index.php
+++ jour03/job03/index.php
@@ -1,19 +1,20 @@
 <?php
 
-function Bonjour(){
+$str="I'm sorry Dave I'm afraid I can't
+do that";
+ 
+$voyelle=array('a','o','I','e','u','i');
 
-    return "Bonjour";
-}
 
-    function Bonsoir(){
-     echo  "Bonsoir";
 
+for($i=0;$i<=(isset($str[$i]));$i++){
+    foreach($voyelle as $value){
+        if($value == $str[$i]){
+    
+            echo $str[$i]; 
+
+        }
+       
     }
-     
-$bonjour = Bonjour();
-$bonsoir = Bonsoir();
-
-echo $bonjour;
-echo $bonsoir;
-
+}
 ?>
